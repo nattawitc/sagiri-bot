5e095c905c7feb1bde324403242d8f5af0925d70
Nattawit Chaiworawitsakul <c.nattawit@gmail.com>
2017-07-14 01:27:38 +0700

    Change emoji const back to string

--- emoji/emoji.go
+++ emoji/emoji.go
@@ -1,16 +1,14 @@
 package emoji
 
-type Emoji string
-
 const (
-	Zero  Emoji = "\x30\xE2\x83\xA3" // 0⃣
-	One   Emoji = "\x31\xE2\x83\xA3" // 1⃣
-	Two   Emoji = "\x32\xE2\x83\xA3" // 2⃣
-	Three Emoji = "\x33\xE2\x83\xA3" // 3⃣
-	Four  Emoji = "\x34\xE2\x83\xA3" // 4⃣
-	Five  Emoji = "\x35\xE2\x83\xA3" // 5⃣
-	Six   Emoji = "\x36\xE2\x83\xA3" // 6⃣
-	Seven Emoji = "\x37\xE2\x83\xA3" // 7⃣
-	Eight Emoji = "\x38\xE2\x83\xA3" // 8⃣
-	Nine  Emoji = "\x39\xE2\x83\xA3" // 9⃣
+	Zero  = "\x30\xE2\x83\xA3" // 0⃣
+	One   = "\x31\xE2\x83\xA3" // 1⃣
+	Two   = "\x32\xE2\x83\xA3" // 2⃣
+	Three = "\x33\xE2\x83\xA3" // 3⃣
+	Four  = "\x34\xE2\x83\xA3" // 4⃣
+	Five  = "\x35\xE2\x83\xA3" // 5⃣
+	Six   = "\x36\xE2\x83\xA3" // 6⃣
+	Seven = "\x37\xE2\x83\xA3" // 7⃣
+	Eight = "\x38\xE2\x83\xA3" // 8⃣
+	Nine  = "\x39\xE2\x83\xA3" // 9⃣
 )
